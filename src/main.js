8864575fe63d59f96290370f8cd67425e3f6bc64
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2018-01-17 17:18:35 +0900

    style & action add

--- src/main.js
+++ src/main.js
@@ -2,9 +2,88 @@ import React from "react";
 import ReactDom from "react-dom";
 
 class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false
+    }
+  }
+
+    styles() {
+    return {
+      container: {
+        fontFamily: "helvetica, arial, 'hiragino kaku gothic pro', meiryo, 'ms pgothic', sans-serif",
+        fontSize: 11
+      },
+      like: {
+        display: "inline-block",
+        background: "#3b5998",
+        padding: "0px 5px",
+        borderRadius: 2,
+        color: "#ffffff",
+        cursor: "pointer",
+        float: "left",
+        height: 20,
+        lineHeight: "20px"
+      },
+      likeHover: {
+        background: "#444"
+      },
+      counterBefore: {
+        display: "block",
+        float: "left",
+        width: 6,
+        height: 6,
+        background: "#fafafa",
+        marginLeft: "-12px",
+        borderRight: 10,
+        transform: "rotate(45deg)",
+        WebkitTransform: "rotate(45deg)",
+        marginTop: 6,
+        borderLeft: "1px solid #aaa",
+        borderBottom: "1px solid #aaa"
+      },
+      counter: {
+        display: "block",
+        background: "#fafafa",
+        boxSizing: "border-box",
+        border: "1px solid #aaa",
+        float: "left",
+        padding: "0px 8px",
+        borderRadius: 2,
+        marginLeft: 8,
+        height: 20,
+        lineHeight: "20px"
+      }
+    };
+  }
+
+  // カーソルが乗った時に状態を変更するイベントハンドラ
+  onMouseEnter() {
+    this.setState({hovered: true});
+  }
+
+  // カーソルが外れた時に状態を変更するイベントハンドラ
+  onMouseLeave() {
+    this.setState({hovered: false});
+  }
+
   render() {
+    const styles = this.styles();
+    console.log(this.state);
+
     return (
-      <span>いいねボタン予定地</span>
+      <span style={styles.container}>
+        <span
+          style={styles.like}
+          onMouseEnter={::this.onMouseEnter}
+          onMouseLeave={::this.onMouseLeave}>いいね!
+        </span>
+        <span style={styles.counter}>
+          <span style={styles.counterBefore}>{" "}</span>
+          999
+        </span>
+      </span>
     );
   }
 }
